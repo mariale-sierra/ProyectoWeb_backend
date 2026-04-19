c77e3b8210ec9f1db1b6546827f33e237e5f1f95
doech <sie24405@uvg.edu.gt>
2026-04-18 23:49:02 -0600

    CORS headers

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -7,11 +7,14 @@ import (
 	"ProyectoWeb_backend/db"
 )
 
-func GetSeries(conn *sql.DB) http.HandlerFunc {
+func GetSeries(database *sql.DB) http.HandlerFunc {
     return func(w http.ResponseWriter, r *http.Request) {
 
-        series, err := db.GetAllSeries(conn)
+        w.Header().Set("Access-Control-Allow-Origin", "*")
+        w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+        w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
+        series, err := db.GetAllSeries(database)
         if err != nil {
             http.Error(w, "Error interno", 500)
             return
